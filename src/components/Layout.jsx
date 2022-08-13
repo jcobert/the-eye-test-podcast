d0406ff8b0b82f19deaadf34397e52fc18e39514
Josh Cobert <jdcobert@gmail.com>
2022-08-12 21:06:56 -0400

    add sticky player

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,14 +4,48 @@ import Footer from "./Footer";
 import SpotifyPlayer from "./SpotifyPlayer";
 
 function Layout({ children }) {
+  const [atBottom, setAtBottom] = React.useState(false);
+  // let atBottom = false;
+
+  const handleScroll = () => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight;
+
+    if (bottom) {
+      setAtBottom(true);
+    } else {
+      setAtBottom(false);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="flex-grow font-jost">
       <Header />
       <div className="min-h-screen flex flex-col">
-        <div className="lg:max-w-7xl lg:w-11/12 mx-auto mt-24 sm:mt-32 md:mt-40 px-2 sm:px-8 md:px-10 lg:px-0">
+        <div
+          className="lg:max-w-7xl lg:w-11/12 mx-auto mt-24 sm:mt-32 md:mt-40 px-2 sm:px-8 md:px-10 lg:px-0"
+          onScroll={handleScroll}
+        >
           {children}
         </div>
-        <SpotifyPlayer />
+        <div
+          className={`absolute w-full lg:w-9/12 lg:left-[12.5%] top-full -translate-y-full ${
+            atBottom ? "" : ""
+          }`}
+        >
+          <SpotifyPlayer />
+        </div>
       </div>
       <Footer />
     </div>
--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -5,14 +5,16 @@ function SpotifyPlayer(props) {
     let element = document.getElementById("embed-iframe");
     let options = {
       uri: "spotify:episode:2PbJGxK2fwvxQjXOapiJAk?si=9a5862a069874c73",
+      height: "40%",
+      width: "100%",
     };
     let callback = (EmbedController) => {};
     IFrameAPI.createController(element, options, callback);
   };
 
   return (
-    <div className="">
-      <div id="embed-iframe"></div>
+    <div className="mx-auto">
+      <div id="embed-iframe" className="mx-auto"></div>
     </div>
   );
 }
