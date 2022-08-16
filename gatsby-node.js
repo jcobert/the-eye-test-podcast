f3373fcbbbfa3e1aa16b8829a80af92e98976ea0
Josh Cobert <jdcobert@gmail.com>
2022-08-16 08:49:31 -0400

    create blog post pages by slug

---
+++ gatsby-node.js
@@ -0,0 +1,46 @@
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const blogPost = path.resolve('./src/templates/BlogPost.jsx')
+  const result = await graphql(
+    `
+      {
+        allContentfulBlogPost {
+          nodes {
+            title
+            slug
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful posts`,
+      result.errors
+    )
+    return
+  }
+
+  const posts = result.data.allContentfulBlogPost.nodes
+
+  if (posts.length > 0) {
+    posts.forEach((post, index) => {
+      const previousPostSlug = index === 0 ? null : posts[index - 1].slug
+      const nextPostSlug =
+        index === posts.length - 1 ? null : posts[index + 1].slug
+
+      createPage({
+        path: `/blog/${post.slug}/`,
+        component: blogPost,
+        context: {
+          slug: post.slug,
+          previousPostSlug,
+          nextPostSlug,
+        },
+      })
+    })
+  }
+}
