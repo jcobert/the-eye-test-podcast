366a61a5c75f4c528bdece7bff95192039c4a3d2
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-08-15 20:42:52 -0400

    add dependencies and config (#4)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: `The Eye Test Podcast`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    'gatsby-plugin-postcss',
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST,
+      },
+    },
   ],
-}
+};
