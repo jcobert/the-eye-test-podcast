0f94df5e1d311226d5865f3b7f8ac7a71b5448af
Josh Cobert <jdcobert@gmail.com>
2022-08-15 23:41:21 -0400

    structure and render post previews

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import Heading from "../components/Heading.jsx";
+import PostPreview from "../components/PostPreview.jsx";
+import { graphql } from "gatsby";
 
-function Blog() {
+function Blog({ data }) {
+  const posts = data.allContentfulBlogPost.edges;
   return (
     <div>
       <Heading
         title={"Blog"}
-        subtitle={"Insight from The Eye Test contributors."}
+        subtitle={"Hot takes and  from The Eye Test contributors."}
       />
+      <div className="w-full flex flex-col gap-y-16 mb-24">
+        {posts.map(({ node, index }) => {
+          return <PostPreview key={index} post={node} />;
+        })}
+      </div>
     </div>
   );
 }
+
+export const query = graphql`
+  query {
+    allContentfulBlogPost(sort: {fields: publishDate, order: DESC}) {
+      edges {
+        node {
+          title
+          description {
+            raw
+          }
+          author {
+            name
+          }
+          heroImage {
+            url
+          }
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+        }
+      }
+    }
+  }
+`;
 
 export default Blog;
 
