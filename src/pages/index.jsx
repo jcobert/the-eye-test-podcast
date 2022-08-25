4f0859db95e2fdcf4d5c68e53e2727fc5e8b23bc
Josh Cobert <jdcobert@gmail.com>
2022-08-24 23:08:41 -0400

    being content layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,27 +19,48 @@ function Home({ data }) {
         title={"The Eye Test"}
         subtitle={"Welcome to the official home of The Eye Test Podcast."}
       />
-      <div className="flex flex-col gap-y-16">
-        {/* Apple Podcast Player */}
-        <div>
-          {/* <h3 className="text-xl md:text-2xl text-center text-slate-600 mb-4 md:mb-8">
-            Listen to The Eye Test Podcast
-          </h3> */}
-          <iframe
-            className="w-full max-w-md mx-auto rounded-sm overflow-hidden"
-            src="https://embed.podcasts.apple.com/us/podcast/the-eye-test-podcast/id1611984184?itsct=podcast_box_player&amp;itscg=30200&amp;ls=1&amp;theme=auto"
-            height="450px"
-            frameborder="0"
-            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
-            allow="autoplay *; encrypted-media *; clipboard-write"
-          ></iframe>
+      <div className="flex flex-col gap-y-16 md:gap-y-20 lg:gap-y-28">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 md:gap-x-4 gap-y-10 justify-items-stretch">
+          {/* Apple Podcast Player */}
+          <div className="md:col-start-2 lg:col-start-3 lg:col-span-3">
+            <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
+              Catch Up on the Latest Episodes
+            </h3>
+            <iframe
+              className="w-full max-w-md sm:max-w-xl mx-auto md:mx-0 border border-slate-400 shadow rounded-sm overflow-hidden"
+              src="https://embed.podcasts.apple.com/us/podcast/the-eye-test-podcast/id1611984184?itsct=podcast_box_player&amp;itscg=30200&amp;ls=1&amp;theme=auto"
+              height="452px"
+              width="650px"
+              frameborder="0"
+              sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+              allow="autoplay *; encrypted-media *; clipboard-write"
+            ></iframe>
+          </div>
+          {/* Placeholder */}
+          <div className="col-start-1 col-span-1 lg:col-span-2 row-start-1">
+            <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
+              Placeholder
+            </h3>
+            <div className="border border-slate-400">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                nec ante id ligula rutrum blandit. Nunc blandit vehicula congue.
+                Cras luctus, neque nec sodales euismod, mi ipsum facilisis ex,
+                non lacinia nunc felis sit amet neque. Aliquam a libero enim.
+                Aliquam magna odio, gravida eget blandit a, convallis non
+                mauris. Fusce ultricies nisi suscipit consequat ornare. Nullam
+                scelerisque libero a augue consectetur, ac commodo massa
+                suscipit.
+              </p>
+            </div>
+          </div>
         </div>
         {/* Featured Blog Posts */}
-        <div>
-          <h3 className="text-xl md:text-2xl text-center text-slate-600 mb-4 md:mb-8">
+        <div className="w-full col-span-full">
+          <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
             Recent Blog Posts
           </h3>
-          <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-8 mb-24">
+          <div className="w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-y-12 gap-x-8 mb-24">
             {postPreviews.slice(0, 3)}
           </div>
         </div>
