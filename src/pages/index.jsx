af362361d2df0a2850f3ae32d3de98f82c3658f0
Josh Cobert <jdcobert@gmail.com>
2022-08-25 08:42:30 -0400

    revise content layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,14 +20,14 @@ function Home({ data }) {
         subtitle={"Welcome to the official home of The Eye Test Podcast."}
       />
       <div className="flex flex-col gap-y-16 md:gap-y-20 lg:gap-y-28">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 md:gap-x-4 gap-y-10 justify-items-stretch">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 md:gap-x-4 gap-y-16 justify-items-stretch">
           {/* Apple Podcast Player */}
           <div className="md:col-start-2 lg:col-start-3 lg:col-span-3">
-            <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
+            {/* <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
               Catch Up on the Latest Episodes
-            </h3>
+            </h3> */}
             <iframe
-              className="w-full max-w-md sm:max-w-xl mx-auto md:mx-0 border border-slate-400 shadow rounded-sm overflow-hidden"
+              className="w-full max-w-3xl mx-auto border border-slate-400 shadow rounded-sm overflow-hidden"
               src="https://embed.podcasts.apple.com/us/podcast/the-eye-test-podcast/id1611984184?itsct=podcast_box_player&amp;itscg=30200&amp;ls=1&amp;theme=auto"
               height="452px"
               width="650px"
@@ -38,9 +38,9 @@ function Home({ data }) {
           </div>
           {/* Placeholder */}
           <div className="col-start-1 col-span-1 lg:col-span-2 row-start-1">
-            <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
+            {/* <h3 className="text-xl md:text-2xl text-center lg:text-left text-slate-600 mb-4 lg:mb-8">
               Placeholder
-            </h3>
+            </h3> */}
             <div className="border border-slate-400">
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
