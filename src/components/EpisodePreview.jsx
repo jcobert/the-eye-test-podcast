db07e1d0b0aac4aa4465b174b73e10cf72291b59
Josh Cobert <jdcobert@gmail.com>
2022-09-03 12:36:41 -0400

    format description text

--- src/components/EpisodePreview.jsx
+++ src/components/EpisodePreview.jsx
@@ -7,26 +7,27 @@ import {
 function EpisodePreview(props) {
   const dispatch = useContext(GlobalDispatchContext);
   const episode = props.node;
-  const description = episode.description.replace(
-    episode.title,
-    ""
-  );
+  const description = episode.description
+    .replace(episode.title, "")
+    .replaceAll("\n", "<br>");
 
   return (
     <div>
-      <h3>{episode.title}</h3>
-      <p>{description}</p>
-      <button
-        className="bg-slate-500 text-white border border-slate-600 rounded shadow-md p-2"
-        onClick={() => {
-          dispatch({
-            type: "LOAD_EPISODE",
-            payload: episode.simplecastId,
-          });
-        }}
-      >
-        {episode.title}
-      </button>
+      <div className="w-full p-2 text-center bg-slate-50 rounded border border-slate-400 shadow-md">
+        <h3>{episode.title}</h3>
+        <p dangerouslySetInnerHTML={{ __html: description }}></p>
+        <button
+          className="w-8/12 bg-slate-500 text-white border border-slate-600 rounded shadow-md p-2"
+          onClick={() => {
+            dispatch({
+              type: "LOAD_EPISODE",
+              payload: episode.simplecastId,
+            });
+          }}
+        >
+          Listen
+        </button>
+      </div>
     </div>
   );
 }
