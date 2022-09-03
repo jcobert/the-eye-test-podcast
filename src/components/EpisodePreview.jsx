128567ed564ee6b7a23c568eeda56dce56fd3300
Josh Cobert <jdcobert@gmail.com>
2022-09-03 13:18:31 -0400

    structure preview card

--- src/components/EpisodePreview.jsx
+++ src/components/EpisodePreview.jsx
@@ -1,32 +1,48 @@
 import React, { useContext } from "react";
-import {
-  GlobalStateContext,
-  GlobalDispatchContext,
-} from "../context/GlobalContextProvider.jsx";
+import { GlobalDispatchContext } from "../context/GlobalContextProvider.jsx";
 
 function EpisodePreview(props) {
   const dispatch = useContext(GlobalDispatchContext);
   const episode = props.node;
-  const description = episode.description
-    .replace(episode.title, "")
-    .replaceAll("\n", "<br>");
+  let description = episode.description.replace(episode.title, "").trim();
+  description = description.replaceAll("\n", "<br>");
 
   return (
     <div>
-      <div className="w-full p-2 text-center bg-slate-50 rounded border border-slate-400 shadow-md">
-        <h3>{episode.title}</h3>
-        <p dangerouslySetInnerHTML={{ __html: description }}></p>
-        <button
-          className="w-8/12 bg-slate-500 text-white border border-slate-600 rounded shadow-md p-2"
-          onClick={() => {
-            dispatch({
-              type: "LOAD_EPISODE",
-              payload: episode.simplecastId,
-            });
-          }}
-        >
-          Listen
-        </button>
+      <div className="w-full flex flex-col gap-y-1 p-2 text-center bg-slate-50 rounded border border-slate-400 shadow-md">
+        {/* Title */}
+        <div>
+          <h3 className="text-xl font-semibold text-theme-primary">
+            {episode.title}
+          </h3>
+        </div>
+        {/* Date */}
+        <div>
+          <p className="text-slate-600 text-sm">{episode.publishedAt}</p>
+        </div>
+        {/* Description */}
+        <div>
+          <p
+            className="text-left p-4"
+            dangerouslySetInnerHTML={{ __html: description }}
+          ></p>
+        </div>
+        {/* Tags */}
+        <div></div>
+        {/* Listen Button */}
+        <div>
+          <button
+            className="w-8/12 bg-slate-300 text-slate-800 border border-slate-600 rounded shadow-md p-2"
+            onClick={() => {
+              dispatch({
+                type: "LOAD_EPISODE",
+                payload: episode.simplecastId,
+              });
+            }}
+          >
+            Listen
+          </button>
+        </div>
       </div>
     </div>
   );
