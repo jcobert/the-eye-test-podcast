c9b225c72b6eaa7e2698cb5ced08ea79c3a66765
Josh Cobert <jdcobert@gmail.com>
2022-09-16 09:38:02 -0400

    add hide-show controls

--- src/components/GlobalPlayer.jsx
+++ src/components/GlobalPlayer.jsx
@@ -4,7 +4,11 @@ import {
   GlobalDispatchContext,
 } from "../context/GlobalContextProvider.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark } from "@fortawesome/pro-regular-svg-icons";
+import {
+  faCircleXmark,
+  faUpRightAndDownLeftFromCenter,
+  faDownLeftAndUpRightToCenter,
+} from "@fortawesome/pro-regular-svg-icons";
 
 function GlobalPlayer() {
   const state = useContext(GlobalStateContext);
@@ -12,26 +16,66 @@ function GlobalPlayer() {
 
   return (
     <div
-      className={`sticky bottom-0 mx-auto w-full lg:w-8/12 max-w-6xl lg:rounded-t-md shadow-md ${
+      id="player-container"
+      className={`sticky bottom-0 mx-auto w-full lg:w-8/12 max-w-6xl lg:rounded-t-md p-2 ${
         state.uri === "" ? "hidden" : ""
       }`}
     >
-      <div className="my-auto bg-slate-100 border-t-2 border-slate-300">
-        <div className="float-right">
+      <div className="my-auto bg-theme-primary border border-slate-400 rounded-md shadow-md">
+        {/* Hide, show, close controls */}
+        <div className="p-1">
           <button
+            id="btn-hide-show"
             onClick={() => {
-              dispatch({
-                type: "UNLOAD_EPISODE",
-              });
+              document.getElementById("iframe").classList.toggle("hidden");
+              if (
+                document.getElementById("iframe").className.includes("hidden")
+              ) {
+                document
+                  .getElementById("icon-collapse")
+                  .classList.add("hidden");
+                document
+                  .getElementById("icon-expand")
+                  .classList.remove("hidden");
+              } else {
+                document
+                  .getElementById("icon-expand")
+                  .classList.add("hidden");
+                document
+                  .getElementById("icon-collapse")
+                  .classList.remove("hidden");
+              }
             }}
           >
             <FontAwesomeIcon
-              icon={faCircleXmark}
-              className="text-xl text-slate-600 px-2 pt-1"
+              id="icon-expand"
+              icon={faUpRightAndDownLeftFromCenter}
+              className="text-xl text-slate-100 px-2 hidden"
+            />
+            <FontAwesomeIcon
+              id="icon-collapse"
+              icon={faDownLeftAndUpRightToCenter}
+              className="text-xl text-slate-100 px-2"
             />
           </button>
+          <div className="float-right">
+            <button
+              onClick={() => {
+                dispatch({
+                  type: "UNLOAD_EPISODE",
+                });
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                className="text-xl text-slate-100 px-2"
+              />
+            </button>
+          </div>
         </div>
         <iframe
+          id="iframe"
+          className="rounded-md rounded-t-none"
           height="200px"
           width="100%"
           frameborder="no"
