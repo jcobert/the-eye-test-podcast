65cb33b79fac4c185945ef35eabac51df78ef521
Josh Cobert <jdcobert@gmail.com>
2022-10-09 15:16:39 -0400

    update deps

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,37 +1,7 @@
 import "./src/styles/global.css";
-import React from "react";
 import Layout from "./src/components/Layout";
-// import { Script } from "gatsby";
+import React from "react";
 import GlobalContextProvider from "./src/context/GlobalContextProvider";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      key="jost"
-      rel="preload"
-      href="/fonts/Jost-VariableFont_wght.ttf"
-      as="font"
-      type="font/ttf"
-      crossOrigin="anonymous"
-    />,
-    <link
-      key="optician"
-      rel="preload"
-      href="/fonts/Optiker-K.woff2"
-      as="font"
-      type="font/woff2"
-      crossOrigin="anonymous"
-    />,
-    <link
-      key="optician"
-      rel="preload"
-      href="/fonts/Optiker-K.woff"
-      as="font"
-      type="font/woff"
-      crossOrigin="anonymous"
-    />,
-  ]);
-};
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -19,14 +19,14 @@ export const onRenderBody = ({ setHeadComponents }) => {
       type="font/woff2"
       crossOrigin="anonymous"
     />,
-    <link
-      key="optician"
-      rel="preload"
-      href="/fonts/Optiker-K.woff"
-      as="font"
-      type="font/woff"
-      crossOrigin="anonymous"
-    />,
+    // <link
+    //   key="optician"
+    //   rel="preload"
+    //   href="/fonts/Optiker-K.woff"
+    //   as="font"
+    //   type="font/woff"
+    //   crossOrigin="anonymous"
+    // />,
   ]);
 };
 
