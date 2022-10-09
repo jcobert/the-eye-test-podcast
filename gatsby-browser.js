472fc356597f6e1f644aeed848ed7d33c47b049b
Josh Cobert <jdcobert@gmail.com>
2022-10-09 14:42:04 -0400

    add font links to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,35 @@ import Layout from "./src/components/Layout";
 // import { Script } from "gatsby";
 import GlobalContextProvider from "./src/context/GlobalContextProvider";
 
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="jost"
+      rel="preload"
+      href="/fonts/Jost-VariableFont_wght.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="optician"
+      rel="preload"
+      href="/fonts/Optiker-K.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key="optician"
+      rel="preload"
+      href="/fonts/Optiker-K.woff"
+      as="font"
+      type="font/woff"
+      crossOrigin="anonymous"
+    />,
+  ]);
+};
+
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
